29ff68d14d0fbafe80b218ce68111e78348b1b13
Luke Encrapera <luke.encrapera@gianteagle.com>
2021-01-11 10:28:44 -0500

    Create installEXOmodule.ps1

---
+++ installEXOmodule.ps1
@@ -0,0 +1,6 @@
+﻿#Installs exchange online and updates module on run. 
+Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+Install-Module -Name ExchangeOnlineManagement -scope CurrentUser
+Import-Module ExchangeOnlineManagement; Get-Module ExchangeOnlineManagement
+Update-Module -Name ExchangeOnlineManagement
+#Uninstall-Module -Name ExchangeOnlineManagement
